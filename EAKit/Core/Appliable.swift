0b30da1894416a44f06bed2fac97219ca14ffb03
Meniny <meniny@qq.com>
2018-05-23 21:24:22 +0800

    1.5.6

--- EAKit/Core/Appliable.swift
+++ EAKit/Core/Appliable.swift
@@ -15,6 +15,48 @@ public extension Appliable {
         closure(self)
         return self
     }
+    
+    @discardableResult
+    public static func apply(_ items: Array<Self>, closure: (Self) -> Void) -> Array<Self> {
+        return items.apply(closure: closure)
+    }
+    
+    @discardableResult
+    public static func apply(_ items: Self..., closure: (Self) -> Void) -> Array<Self> {
+        return apply(items, closure: closure)
+    }
+}
+
+public extension Array where Element: Appliable {
+    @discardableResult
+    public func apply(closure: (Element) -> Void) -> Array<Element> {
+        forEach(closure)
+        return self
+    }
 }
 
 extension NSObject: Appliable {}
+
+extension Dictionary: Appliable {}
+extension Array: Appliable {}
+
+extension Bool: Appliable {}
+extension String: Appliable {}
+extension CGFloat: Appliable {}
+extension CGImage: Appliable {}
+extension Float: Appliable {}
+extension Double: Appliable {}
+
+extension Int: Appliable {}
+extension Int8: Appliable {}
+extension Int16: Appliable {}
+extension Int32: Appliable {}
+extension Int64: Appliable {}
+
+extension UInt: Appliable {}
+extension UInt8: Appliable {}
+extension UInt16: Appliable {}
+extension UInt32: Appliable {}
+extension UInt64: Appliable {}
+
+
